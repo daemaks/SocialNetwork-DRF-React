ade3f3a76d4258cdf9bd0b29e69dc7dbd060129c
Maksym Lytvynov <litvinov.work7@gmail.com>
2022-10-29 18:50:02 +0300

    test user login & register

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -8,4 +8,39 @@ client = APIClient()
 
 @pytest.mark.django_db
 def test_register_user():
-    pass
+    payload = {
+        "email": "example@mail.com",
+        "username": "example",
+        "password": "example",
+    }
+    response = client.post("http://127.0.0.1:8000/api/user/register/", payload)
+
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_register_user_fail():
+    payload = {
+        "username": "example",
+        "password": "example",
+    }
+    response = client.post("http://127.0.0.1:8000/api/user/register/", payload)
+
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_login_user():
+    payload = {
+        "email": "example@mail.com",
+        "username": "example",
+        "password": "example",
+    }
+    client.post("http://127.0.0.1:8000/api/user/register/", payload)
+
+    response = client.post(
+        "http://127.0.0.1:8000/api/token/",
+        {"email": "example@mail.com", "password": "example"},
+    )
+
+    assert response.status_code == 200
