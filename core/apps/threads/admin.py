a9b8fcf2b3732f99408c223e147c9dd5a9981947
daemaks <litvinov.work7@gmail.com>
2022-11-04 19:19:16 +0200

    threads admin panel

--- core/apps/threads/admin.py
+++ core/apps/threads/admin.py
@@ -1,3 +1,8 @@
+from core.apps.threads.models import Comment, Community, Likes, Tag, Thread
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Community)
+admin.site.register(Thread)
+admin.site.register(Comment)
+admin.site.register(Likes)
