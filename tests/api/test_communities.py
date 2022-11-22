4ba1f04825a8a44d9026d0b77feb875ce6a6cc79
daemaks <litvinov.work7@gmail.com>
2022-11-22 17:20:53 +0200

    test community list

--- tests/api/test_communities.py
+++ tests/api/test_communities.py
@@ -19,3 +19,12 @@ def test_tag_details(api_community, client):
     url = "http://127.0.0.1:8000/api/threads/tag/1/"
     response = client.get(url)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_community_list(api_community, client):
+    url = reverse("community_list")
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert len(response.data) == 1
