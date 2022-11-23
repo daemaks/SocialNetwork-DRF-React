18001207cc3473acfc5c194536bbc85a2f22fd6b
daemaks <litvinov.work7@gmail.com>
2022-11-23 02:21:31 +0200

    test treads list

--- tests/api/test_threads.py
+++ tests/api/test_threads.py
@@ -1,9 +1,13 @@
 import pytest
 from rest_framework.test import APIClient
+from django.urls import reverse
 
 client = APIClient()
 
 
-# @pytest.mark.django_db
-# def test_threads_of_community(db, api_thread):
-#     pass
+@pytest.mark.django_db
+def test_threads_list(db, api_thread):
+    url = reverse("treads_list")
+    response = client.get(url)
+
+    assert response.status_code == 200
