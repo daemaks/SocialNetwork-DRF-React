2e92763da4dda6b5982ccf66099e2e2c094db30d
daemaks <litvinov.work7@gmail.com>
2022-11-24 06:15:19 +0200

    update threads tests

--- tests/api/test_threads.py
+++ tests/api/test_threads.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
+from core.apps.threads.models import Thread
 
 client = APIClient()
 
@@ -8,10 +9,15 @@ client = APIClient()
 @pytest.mark.django_db
 def test_thread_create(api_account, api_community):
     url = reverse("threads_create")
-    data = {"title": "example", "community": 1}
-    response = api_account.post(url, data)
+    payload = {"title": "example", "community": 1}
+    response = api_account.post(url, payload)
+    data = response.data
+    data_from_db = Thread.objects.all().first()
 
-    assert response.status_code == 201
+    assert data["id"] == data_from_db.id
+    assert data["title"] == data_from_db.title
+    assert data["community"] == data_from_db.community.id
+    assert data["username"] == data_from_db.username.id
 
 
 @pytest.mark.django_db
@@ -20,6 +26,7 @@ def test_threads_list(db, api_thread):
     response = client.get(url)
 
     assert response.status_code == 200
+    assert len(response.data) == 1
 
 
 @pytest.mark.django_db
@@ -28,6 +35,7 @@ def test_threads_list_of_community(db, api_thread):
     response = client.get(url)
 
     assert response.status_code == 200
+    assert len(response.data) == 1
 
 
 @pytest.mark.django_db
