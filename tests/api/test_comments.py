1e0ef39402a80fd49410c1fb48c5ad71c8f54980
daemaks <litvinov.work7@gmail.com>
2022-11-26 14:07:04 +0200

    test another comment update

--- tests/api/test_comments.py
+++ tests/api/test_comments.py
@@ -25,11 +25,38 @@ def test_comments_retrieve(api_account, api_community):
     assert response.status_code == 200
 
 
+"""UPDATE"""
+
+
 @pytest.mark.django_db
-def test_comments_update(api_account, api_community):
+def test_comments_update_by_owner(api_account, api_community):
     Thread.objects.create(username_id=1, title="example", community_id=1)
     Comment.objects.create(username_id=1, thread_id=1, text="example")
     url = reverse("comment_details", kwargs={"pk": "1"})
     response = api_account.put(url, {"text": "testtext"})
 
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_comments_update_by_other_user(
+    api_account, api_account_2, api_community
+):
+    Thread.objects.create(username_id=1, title="example", community_id=1)
+    Comment.objects.create(username_id=1, thread_id=1, text="example")
+    url = reverse("comment_details", kwargs={"pk": "1"})
+    response = api_account_2.put(url, {"text": "testtext"})
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_comments_update_by_admin(
+    api_account, api_admin_account, api_community
+):
+    Thread.objects.create(username_id=1, title="example", community_id=1)
+    Comment.objects.create(username_id=1, thread_id=1, text="example")
+    url = reverse("comment_details", kwargs={"pk": "1"})
+    response = api_admin_account.put(url, {"text": "testtext"})
+
+    assert response.status_code == 403
