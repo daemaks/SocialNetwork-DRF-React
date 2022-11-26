23eff76d0ad8c3e69e80aa0ce3efe79f3f17b7a0
daemaks <litvinov.work7@gmail.com>
2022-11-26 14:11:23 +0200

    test destroy comment

--- tests/api/test_comments.py
+++ tests/api/test_comments.py
@@ -60,3 +60,40 @@ def test_comments_update_by_admin(
     response = api_admin_account.put(url, {"text": "testtext"})
 
     assert response.status_code == 403
+
+
+"""DESTROY"""
+
+
+@pytest.mark.django_db
+def test_comments_destroy_by_owner(api_account, api_community):
+    Thread.objects.create(username_id=1, title="example", community_id=1)
+    Comment.objects.create(username_id=1, thread_id=1, text="example")
+    url = reverse("comment_details", kwargs={"pk": "1"})
+    response = api_account.delete(url)
+
+    assert response.status_code == 204
+
+
+@pytest.mark.django_db
+def test_comments_destroy_by_other_user(
+    api_account, api_account_2, api_community
+):
+    Thread.objects.create(username_id=1, title="example", community_id=1)
+    Comment.objects.create(username_id=1, thread_id=1, text="example")
+    url = reverse("comment_details", kwargs={"pk": "1"})
+    response = api_account_2.delete(url)
+
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_comments_destroy_by_admin(
+    api_account, api_admin_account, api_community
+):
+    Thread.objects.create(username_id=1, title="example", community_id=1)
+    Comment.objects.create(username_id=1, thread_id=1, text="example")
+    url = reverse("comment_details", kwargs={"pk": "1"})
+    response = api_admin_account.delete(url)
+
+    assert response.status_code == 204
