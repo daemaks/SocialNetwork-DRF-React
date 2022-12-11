66e87c75b8c3e812811d8b73e059c7e10cc9efb0
daemaks <litvinov.work7@gmail.com>
2022-12-11 16:45:00 +0200

    axiosInstance

---
+++ client/src/axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const baseURl = 'http://127.0.0.1:8000/api';
+
+const axiosInstance = axios.create({
+    baseURL: baseURl,
+    timeout: 5000,
+    headers: {
+        Authorization: localStorage.getItem('access_token')
+            ? 'Bearer ' + localStorage.getItem('access_token')
+            : null,
+    },
+});
