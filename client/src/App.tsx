c5c994bf24d4703503aecb350ca889aae4acf1e7
daemaks <litvinov.work7@gmail.com>
2022-12-14 00:40:30 +0200

    login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import {Routes, Route} from 'react-router-dom'
 import {Header} from './components/Header';
 import {Footer} from './components/Footer';
 import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
 import SignUp from './components/register';
+import SignIn from './components/login';
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<MainPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/login' element={<SignIn/>}/>
         <Route path='/register' element={<SignUp/>}/>
       </Routes>
       <Footer/>
--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -2,11 +2,10 @@ import React, {useState} from "react";
 import axiosInstance from '../axios';
 import { useNavigate } from 'react-router-dom';
 
-export default function SignUp() {
+export default function SignIn() {
     const navigate = useNavigate();
     const initialFormData = Object.freeze({
         email: '',
-        username: '',
         password:'',
     });
 
@@ -18,24 +17,27 @@ export default function SignUp() {
             [event.target.name]: event.target.value.trim(),
         });
     };
-
     const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
 
-        axiosInstance
-            .post(`user/register/`, {
-                email: formData.email,
-                username: formData.username,
-                password: formData.password,
-            })
-            .then((res) => {
-                navigate('/')
-            })
+    axiosInstance
+        .post(`token/`, {
+            email: formData.email,
+            password: formData.password,
+        })
+        .then((res) => {
+            localStorage.setItem('access_token', res.data.access);
+            localStorage.setItem('refresh_token', res.data.refresh);
+            axiosInstance.defaults.headers['Authorization'] =
+                'Bearer ' + localStorage.getItem('access_token');
+            navigate('/')
+            console.log(res.data)
+        });
     };
+
     return (
         <form>
             <input onChange={handleChange} type="text" name='email' placeholder='Enter Email'/>
-            <input onChange={handleChange} type="text" name='username' placeholder='Enter username'/>
             <input onChange={handleChange} type="password" name='password' placeholder='Enter Password'/>
             <button type="submit" onClick={handleSubmit}>Submit</button>
         </form>
