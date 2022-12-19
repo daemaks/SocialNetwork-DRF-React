d9351037af1443d8d195e1672801bb918f566b50
daemaks <litvinov.work7@gmail.com>
2022-12-20 00:59:39 +0200

    some style for header

--- client/src/layouts/Header.tsx
+++ client/src/layouts/Header.tsx
@@ -1,14 +1,23 @@
 import {Link} from 'react-router-dom'
 export function Header() {
     return (
-        <>
-            <Link to="/">Home</Link>
-            <span> | </span>
-            <Link to="/login">Login</Link>
-            <span> | </span>
-            <Link to="/logout">Logout</Link>
-            <span> | </span>
-            <Link to="/register">SignUp</Link>
-        </>
+        <div className='flex items-center bg-neutral-400 h-12 w-full px-5'>
+            <div className='mr-96'>
+                <Link to="/">Home</Link>
+            </div>
+            <div className='w-6/12'>
+                <label className="relative block">
+                    <input
+                        type="search" 
+                        placeholder='Search'
+                        className='w-9/12 px-3 py-2 rounded-3xl' />
+                </label>
+            </div>
+                <Link to="/login">Login</Link>
+                <span> | </span>
+                <Link to="/logout">Logout</Link>
+                <span> | </span>
+                <Link to="/register">SignUp</Link>
+        </div>
     )
 }
